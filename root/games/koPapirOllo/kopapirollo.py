61413c77dfa84001eb89c4e2b350c93d4e355390
MrErik402 <bujakierikattila@turr.hu>
2023-05-31 14:13:35 +0200

    Képek elkészítése

---
+++ root/games/koPapirOllo/kopapirollo.py
@@ -0,0 +1,13 @@
+from tkinter import*
+import random
+
+# kő = 1, papír = 2, olló = 3
+
+def pick():
+    gép_választás = random.choice(1,3)
+    
+ablak = Tk()
+tx = Label(ablak, text="*A gép választásának helye*")
+can1 = Canvas()
+ollo = a
+ablak.mainloop()
