6aea26a9d6155379c0e54515e7ecfa6255e20c59
MrErik402 <bujakierikattila@turr.hu>
2023-06-05 12:19:37 +0200

    Kőpapír alap

--- root/games/koPapirOllo/kopapirollo.py
+++ root/games/koPapirOllo/kopapirollo.py
@@ -2,12 +2,14 @@ from tkinter import*
 import random
 
 # kő = 1, papír = 2, olló = 3
-
-def pick():
-    gép_választás = random.choice(1,3)
-    
 ablak = Tk()
 tx = Label(ablak, text="*A gép választásának helye*")
-can1 = Canvas()
-ollo = a
+can1 = Canvas(ablak,width=15, height=15, bg='white' )
+can2 = Canvas(ablak,width=15, height=15, bg='white')
+can3 = Canvas(ablak, width=15, height=15, bg='white')
+tippek = ["kő", "papír", "olló"]
+gep_valasztas = random.choice(tippek) # ko v papir v ollúő
+jatek_valasztas = Label(ablak, text="Adja meg a tippjét: ")
+jatek_valasztas_ki = Entry(ablak)
+
 ablak.mainloop()
